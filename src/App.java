102a9d56676f1986f4d712ef8aad35b5da3d938a
vishishtK <vishisht.khilariwal@gmail.com>
2022-10-06 01:01:21 -0400

    Bloom Filter done

--- src/App.java
+++ src/App.java
@@ -1,5 +1,19 @@
+import java.util.Random;
+import java.util.stream.IntStream;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        int NumberOfElements = 1000;
+        int NumberOfBits = 1000;
+        int NumberOfHashes = 3;
+
+        int[]  randomSetA = IntStream.generate(() -> new Random().nextInt()).limit(NumberOfElements).map(flowId -> Math.abs(flowId)).toArray();
+        int[]  randomSetB = IntStream.generate(() -> new Random().nextInt()).limit(NumberOfElements).map(flowId -> Math.abs(flowId)).toArray();
+
+        System.out.println("Testing BloomFilter");
+        BloomFilter multiHashTable = new BloomFilter(NumberOfBits, NumberOfHashes);
+        multiHashTable.ConsumeElements(randomSetA);
+        multiHashTable.Output("BloomFilter.txt",randomSetA,randomSetB);
+
     }
 }
--- src/BloomFilter.java
+++ src/BloomFilter.java
@@ -1,20 +1,55 @@
 public class BloomFilter {
-    int[] bitmap;
+    Integer[] bitmap;
     int numberOfBits;
+    int numberOfHashes;
     HashFunctions hashFunctions;
 
     public BloomFilter(int numberOfBits, int numberOfHashes){
         this.numberOfBits = numberOfBits;
         this.hashFunctions = new HashFunctions(numberOfHashes, numberOfBits);
+        this.numberOfHashes = numberOfHashes;
+        bitmap = new Integer[numberOfBits];
     }
 
-    public void encode(){
-
+    private void encode(int element){
+        for(int i=0;i<numberOfHashes;i++){
+            bitmap[hashFunctions.Hash(element, i)] = 1;
+        }
     }
 
-    public int lookUp(){
-        return 0;
+    private boolean lookUp(int element){
+        for(int i=0;i<numberOfHashes;i++){
+            if((bitmap[hashFunctions.Hash(element, i)] == null) || bitmap[hashFunctions.Hash(element, i)] != 1){
+                return false;
+            }
+        }
+        return true;
     }
 
+    public void ConsumeElements(int[] set){
+        for (int i = 0; i < set.length; i++){
+            encode(set[i]);
+        }
+    }
+
+    public void Output(String outputFileName, int[] randomSetA, int[] randomSetB) {
+        System.out.println("A)");
+        int elementsFoundOfSetA = 0;
+        for(int i=0;i<randomSetA.length;i++){
+            if(lookUp(randomSetA[i])){
+                elementsFoundOfSetA++;
+            }
+        }
+        System.out.println("Number of elements of set A found in filter: " + elementsFoundOfSetA);
+
+        System.out.println("B)");
+        int elementsFoundOfSetB = 0;
+        for(int i=0;i<randomSetB.length;i++){
+            if(lookUp(randomSetB[i])){
+                elementsFoundOfSetB++;
+            }
+        }
+        System.out.println("Number of elements of set B found in filter: " + elementsFoundOfSetB);
+    }
 
 }
